a4580180909a277198c8adb2d55bc310e359a84c
rosslojek <rosslojek7@gmail.com>
2023-06-17 15:55:28 -0400

    test commit

---
+++ test_typescript.ts
@@ -0,0 +1,26 @@
+﻿// Sample TypeScript code for basic functionality
+
+// Example function to display a greeting
+function greet(name: string) {
+    console.log(`Hello, ${name}!`);
+}
+
+// Example class representing a person
+class Person {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    // Method to display a greeting
+    greet() {
+        console.log(`Hello, my name is ${this.name}.`);
+    }
+}
+
+// Example usage
+greet("John");
+
+const person = new Person("Alice");
+person.greet();
