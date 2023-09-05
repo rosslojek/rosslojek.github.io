2a396d2e31d2b2b3a715df1cb0d89e1b3fdd4587
rosslojek <rosslojek7@gmail.com>
2023-09-05 12:44:21 -0400

    Added test_typescript to src folder

---
+++ personal_site_test/personal_site_test/src/test_typescript.ts
@@ -0,0 +1,28 @@
+﻿// Sample TypeScript code for basic functionality
+
+// This is a change to test if this script is doing anything
+
+// Example function to display a greeting
+function greet(name: string) {
+    console.log(`Hello, ${name}!`);
+}
+
+// Example class representing a person
+class Person {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    // Method to display a greeting
+    greet() {
+        console.log(`Hello, my name is ${this.name}.`);
+    }
+}
+
+// Example usage
+greet("John");
+
+const person = new Person("Alice");
+person.greet();
