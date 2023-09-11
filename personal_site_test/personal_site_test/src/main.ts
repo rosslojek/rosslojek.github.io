e0707fa58583a999d31ff4f11838c1a469ee5e8b
rosslojek <rosslojek7@gmail.com>
2023-09-11 19:45:18 -0400

    Add bio section

--- personal_site_test/personal_site_test/src/main.ts
+++
@@ -1,9 +0,0 @@
-// Get references to the button and the paragraph
-const changeTextButton = document.getElementById('changeTextButton') as HTMLButtonElement;
-const dynamicText = document.getElementById('dynamicText') as HTMLParagraphElement;
-
-// Add a click event listener to the button
-changeTextButton.addEventListener('click', () => {
-    // Change the text of the paragraph
-    dynamicText.textContent = 'Text changed! You clicked the button.';
-});
